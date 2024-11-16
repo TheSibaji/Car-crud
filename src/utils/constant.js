4b608d0c2c0cc997356e05b0abb7e10c8113a772
Sibaji Khillar <sibajikhillar@gmail.com>
2024-11-16 20:17:51 +0530

    update url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,7 @@
-export const API_URL = "http://localhost:5000/api";
-export const API_URL2 = "http://localhost:5000";
+// https://car-management-backend-6cya.onrender.com/
+
+// export const API_URL = "http://localhost:5000/api";
+// export const API_URL2 = "http://localhost:5000";
+
+export const API_URL = "https://car-management-backend-6cya.onrender.com/api";
+export const API_URL2 = "https://car-management-backend-6cya.onrender.com";
